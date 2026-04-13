41e250427a752459e9f98cfe1412dff979ab9459
lalulali <christian.hadianto@gmail.com>
2026-04-13 17:48:58 +0700

    feat: add pm symbol

--- src/core/utils/text-rendering.ts
+++ src/core/utils/text-rendering.ts
@@ -340,6 +340,7 @@ function renderMathToUnicode(latex: string): string {
     '\\\\times': '×',
     '\\\\div': '÷',
     '\\\\cdot': '·',
+    '\\\\pm': '±',
     '\\\\mp': '∓',
     '\\\\oplus': '⊕',
     '\\\\otimes': '⊗',
